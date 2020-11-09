46103878758f72b1b50f18d11e744696a0bae7c2
王雪峰 <w9931125@163.com>
2020-11-09 21:04:26 +0800

    完善

--- src/main/java/com/bibe/crm/utils/TalentImportVerifyHandler.java
+++ src/main/java/com/bibe/crm/utils/TalentImportVerifyHandler.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.StringJoiner;
 
